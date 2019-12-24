a771258524da0eb31dddcd6b16281c6481407c7a
Sonny Mo <sonny3690@gmail.com>
2019-12-23 23:47:58 -0500

    Add scheduler in

---
+++ src/components/datePicker/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import InfiniteCalendar, { Calendar, withRange } from 'react-infinite-calendar';
+import 'react-infinite-calendar/styles.css';
+import styled from 'styled-components';
+import styledMap from 'styled-map'
+
+
+const HeadContainer = styled.div`
+  margin-left: 30px;
+`
+
+const CalendarContainer = styled.div`
+  color: #00000;
+  border-radius: 30px;
+  box-shadow: 0 6px 20px 0 rgba(0, 0, 0, 0.3);
+`
+
+const TextContainer = styled.p`
+  text-align: center;
+  font-size: ${styledMap`
+    header: 2rem;
+    caption: 1rem;
+  `}
+  color: ${styledMap`
+    header: #000000;
+    caption: #AFAFAF;
+  `}
+`
+
+
+const datePicker = ({
+  inputDefaultValue = 'New Event Name',
+  type = 'text'
+}) => {
+
+  const rangeCalendar = withRange(Calendar);
+  let today = new Date();
+
+
+  return (
+    <HeadContainer>
+
+      <div style={{width: '500px'}}>
+        <TextContainer header>What Days Might Work?</TextContainer>
+        <TextContainer caption>Click and drag to choose possibilities.</TextContainer>
+
+        <CalendarContainer>
+          <InfiniteCalendar
+            Component={rangeCalendar}
+            width={500}
+            height={300}
+            selected={today}
+            minDate={today}
+            locale={{
+              headerFormat: 'MMM Do',
+            }}
+          />
+        </CalendarContainer>
+      </div>
+    </HeadContainer>
+  )
+
+}
+
+export default datePicker;
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import Input from '../components/input';
+import DatePicker from '../components/datePicker'
+import styled from 'styled-components'
+
+
+const VerticalSplit = styled.div`
+  width: 100%
+`
+
+
+const HalfPaneContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+`
 
 
 const homePage = () => {
 
-  return (      
+  return (
+    <div>
       <Input />
+      <div
+
+
+      >
+
+        <HalfPaneContainer>
+
+          <DatePicker />
+
+
+          {/* <div style={{ background: 'red', height: '300px', width: '100%' }} /> */}
+
+
+        </HalfPaneContainer>
+
+      </div>
+
+
+
+
+    </div>
   )
 }
 
